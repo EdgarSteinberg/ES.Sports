b467126ceb9700395fcfeadcf6b688ea1c7825f1
¨Edgar <s.steinberg2019@gmail.com>
2023-07-19 15:15:34 -0300

    carrito

--- js/script.js
+++ js/script.js
@@ -1,50 +1,110 @@
 
-//Pre Entrega Edgar Steinberg 
+//Segunda Pre Entrega Proyecto final  Edgar Steinberg 
 
 
-let carrito = "";
-let nuevaOperacion = false;
-let totalCompra = 0;
+// Mi array de objetos con id nombre producto y precio
+let productos = [
+  {
+    id: 1,
+    nombreProducto: "camiseta argentina negra",
+    precio: 24990
+  },
+  {
+    id: 2,
+    nombreProducto: "camiseta argentina blanca",
+    precio: 27000
+  },
+  {
+    id: 3,
+    nombreProducto: "camiseta argentina titular",
+    precio: 53000
+  },
+  {
+    id: 4,
+    nombreProducto: "camiseta argentina violeta",
+    precio: 23989
+  },
+  {
+    id: 5,
+    nombreProducto: "camiseta argentina tres estrellas",
+    precio: 51897
+  },
+  {
+    id: 6,
+    nombreProducto: "camiseta argentina campeona del mundo",
+    precio: 49900
+  }
+];
+// variables globales carrito almacena productos y producto almacena el producto encontrado en el proceso de busqueda.
+let carrito = [];
+let producto;
 
-function hacerCarrito() {
-  do {
-      producto = prompt("Agregar al carrito, seleccione su camiseta\n(Argentina, Independiente, River, Boca):");
-      cantidad = Number(prompt(`Ingrese la cantidad de camisetas de ${producto} que desea comprar:`));
-      seleccionarCamiseta();
-      nuevaOperacion = confirm("¿Desea agregar otro producto al carrito?");
-  } while (nuevaOperacion);
-  alert("Productos en el carrito:\n" + carrito + "Precio total de la compra:\n" + totalCompra + " Pesos\n" + "Gracias por su compra" );
+
+// funcion para interectuar con el usuario donde se le pide que elija el producto con un pront y con otra variable local utilizamos el filter para que busque y filtre que si lo q escribe el usuario esta en mi array y lo almacena en la variable producto.
+function buscarProducto() {
+  let seleccion = prompt("Ingrese el nombre del producto que desea comprar: \ncamiseta argentina negra\ncamiseta argentina blanca\ncamiseta argentina violeta\ncamiseta argentina tres estrella\ncamiseta argentina campeon del mundo");
+  let productosSeleccionados = productos.filter((p) => p.nombreProducto.toLowerCase() == seleccion.toLowerCase());
+
+  if (productosSeleccionados.length > 0) {
+    producto = productosSeleccionados[0];
+  } else {
+    producto = null;
+    alert("El producto seleccionado no existe. Por favor, vuelva a intentarlo.");
+  }
 }
 
-function seleccionarCamiseta() {
-  switch (producto.toLowerCase()) {
-    case "argentina":
-      precio = 33000 * cantidad;
-      carrito += `Camiseta de Argentina - Cantidad: ${cantidad} - Total: ${precio} pesos\n`;
-      totalCompra += precio
+
+//Se le pide al usuario que ingrese la cantidad y se crea un array con la informacion, este obejeto se agrega al carrito.
+function agregarCarrito() {
+  if (producto) {
+    let cantidad = Number(prompt("Ingrese la cantidad que desea comprar"));
+    carrito.push({
+      producto: producto.nombreProducto,
+      cantidad: cantidad,
+      subtotal: producto.precio * cantidad
+    });
+  } 
+}
+
+
+//Hacemos un bucle while seteado en true donde llamamos las dos funciones y colacamos un if con un corfirm negado para que si el usuario apreta aceptar vuelva al bucle y se apreta cancelar sale del con un break.
+function confirmarCarrito() {
+  while (true) {
+    buscarProducto();
+    agregarCarrito();
+    if (!confirm("¿Desea agregar otro producto al carrito?")) {
       break;
-    case "independiente":
-      precio = 27000 * cantidad;
-      carrito += `Camiseta de Independiente - Cantidad: ${cantidad} - Total: ${precio} pesos\n`;
-      totalCompra += precio
-      break;
-    case "river":
-      precio = 25000 * cantidad;
-      carrito += `Camiseta de River - Cantidad: ${cantidad} - Total: ${precio} pesos\n`;
-      totalCompra += precio
-      break;
-    case "boca":
-      precio = 24000 * cantidad
-      carrito += `Camiseta de Boca - Cantidad: ${cantidad} - Total: ${precio} pesos\n`;
-      totalCompra += precio
-      break;
-    default:
-      alert("El producto ingresado no está disponible.");
-      break;
+    }
   }
+}
 
-} 
+//se encarga de calcular y mostrar el total a pagar de un carrito de compras hacemos un foreach que se encarga se recorrer cada elemento del carrito un alert que muestra la cantida el producto y su total el reduce calcula el total que se inicializa en cero sum y item son el acumulador y elemento 
+function calcularTotal() {
+  alert("Carrito de compras: ");
+  carrito.forEach((item) => {
+    alert(`- ${item.cantidad} ${item.producto} : ${item.subtotal}`);
+  });
+  let total = carrito.reduce((sum, item) => sum + item.subtotal, 0);
+  alert(`Total a pagar : ${total}`);
+}
 
-hacerCarrito();  
 
- 
+//carrito se le asigna un array vacio
+function vaciarCarrito() {
+  carrito = [];
+  alert("El carrito ha sido vaciado.");
+}
+
+confirmarCarrito();
+
+
+// aca verifica si por lo menos hay un producto en el carrito que si el usuario apreta aceptar se vaciara el carrito con la funcion vaciarcarrito()
+if (carrito.length > 0) {
+  if (confirm("¿Desea vaciar el carrito?")) {
+    vaciarCarrito();
+  }
+}
+
+calcularTotal();
+
+
